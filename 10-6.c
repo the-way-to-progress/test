2e008795e6bd98a7a27610a8c4d6c3a0b3889ff5
the-way-to-progress <13852313820@163.com>
2022-10-06 11:14:31 +0800

    11

---
+++ 10-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int j;
+    for (i = 0; i < 3;++i)
+        for (j = 0; j < 3; ++j)
+            printf("哈哈\n");
+    printf("嘻嘻\n");
+    return 0;
+}
